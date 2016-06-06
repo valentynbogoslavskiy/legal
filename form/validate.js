ad5788008e81d2e8d7f05a501fc08bbee46a0ce2
valentyn <valentyn.bogoslavskiy@lemberg.co.uk>
2016-06-07 02:18:10 +0300

    Added form.

--- form/validate.js
+++ form/validate.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-$("#submit").click(function() {
-var name = $("#name").val();
-var email = $("#email").val();
-var message = $("#message").val();
-$("#returnmessage").empty(); // To empty previous error/success message.
-// Checking for blank fields.
-if (name == '' || email == '') {
-alert("Будь ласка заповніть всі поля");
-} else {
-// Returns successful data submission message when the entered information is stored in database.
-$.post("process.php", {
-name1: name,
-email1: email,
-message1: message
-}, function(data) {
-$("#returnmessage").append(data); // Append returned message to message paragraph.
-if (data == "Your Query has been received, We will contact you soon.") {
-$("#form")[0].reset(); // To reset form fields on success.
-}
-});
-}
-});
+    $("#submit").click(function() {
+    var name = $("#name").val();
+    var email = $("#email").val();
+    var message = $("#message").val();
+    $("#returnmessage").empty(); // To empty previous error/success message.
+    // Checking for blank fields.
+    if (name == '' || email == '') {
+    alert("Будь ласка заповніть всі поля");
+    } else {
+    // Returns successful data submission message when the entered information is stored in database.
+    $.post("process.php", {
+    name1: name,
+    email1: email,
+    message1: message
+    }, function(data) {
+    $("#returnmessage").append(data); // Append returned message to message paragraph.
+    if (data == "Очікуйте вам перетелефонують") {
+    $("#form")[0].reset(); // To reset form fields on success.
+    }
+    });
+    }
+    });
 });
