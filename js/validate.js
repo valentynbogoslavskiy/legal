fe6e152cdae353cd5001506ec829ab318905f737
Bogoslavskiy valentyn <valentyn.bogoslavskiy@lemberg.co.uk>
2016-06-07 11:27:10 +0300

    Changr structure file.

--- js/validate.js
+++ js/validate.js
@@ -9,13 +9,13 @@ $(document).ready(function() {
     alert("Будь ласка заповніть всі поля");
     } else {
     // Returns successful data submission message when the entered information is stored in database.
-    $.post("process.php", {
+    $.post("../process.php", {
     name1: name,
     email1: email,
     message1: message
     }, function(data) {
     $("#returnmessage").append(data); // Append returned message to message paragraph.
-    if (data == "Очікуйте вам перетелефонують") {
+    if (data == "Очікуйте вам перетелефонують1") {
     $("#form")[0].reset(); // To reset form fields on success.
     }
     });
